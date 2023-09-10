e322f0f541f45928c26306c7e076ed2866b82365
DaltonGuima <dalton.guimaraes500@gmail.com>
2023-09-09 21:13:29 -0300

    Consumption Model Alterado

--- models/Consumption.ts
+++ models/Consumption.ts
@@ -1,12 +1,13 @@
 // Tanto o de agua como o de calorias por aqui
-//  Tem que fazer meio que uma genaralização aqui
+// Tem que fazer meio que uma genaralização aqui
 import mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({
-
-    aguaMl: Number,
-    calorias: Number
-
+    quantidade: Number,
+    tipoConsumo: {
+        type: String,
+        enum: ["Água", "Calorias"]
+    }
 }, { timestamps: true })
 
 export const Consumption = mongoose.model('Consumption', schema)
